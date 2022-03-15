78a0e68c2eefa440ad3c84c0da92f9e041a69a41
Appiva-Tech <brscntyz@gmail.com>
2022-03-15 19:29:16 +0300

    auth and context complete

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,16 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const {error,login} = useLogin()
+
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(email, password)
+    login(email,password)
   }
   
   return (
